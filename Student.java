2d60da29b036716fd32091ed5b4f4f80f07ddf34
Dilshodbekcoder1 <146424839+Dilshodbekcoder1@users.noreply.github.com>
2023-09-30 10:35:49 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,47 @@
+package uz.mohirdev.lesson.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+
+    private Long id;
+
+    private String name;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
